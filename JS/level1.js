cb995db477e49385b66823a4a9f738bc5063e310
EN1993 <74547465+EN1993@users.noreply.github.com>
2021-11-30 14:37:25 +0100

    add

---
+++ JS/level1.js
@@ -0,0 +1,36 @@
+import {books }from"./data/books.js";
+
+const listContainer = document.querySelector("ul");
+
+let bookToRender = books;
+
+function renderBooks() {
+  bookToRender.forEach(function(book) {
+    listContainer.innerHTML +=`<li>isbn: ${book.isbn} Book: ${book.title} <i class="fas fa-trash" data-book="${book.isbn}" data-title="${book.title}"></i>   </li>
+
+    `
+  });
+
+  
+
+  const trashCans = document.querySelectorAll("li i");
+
+  trashCans.forEach(function(can) {
+    can.addEventListener("click", removeFromList);
+    
+  });
+
+
+  function removeFromList(event) {
+    console.log(event);
+    const deleteThisItem = event.target.dataset.book;
+    
+      if (deleteThisItem !== "${book}") {
+        return true;
+      }
+   
+  }
+
+}
+
+renderBooks();
